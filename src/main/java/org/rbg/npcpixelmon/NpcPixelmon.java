9c1c172bf3bd29a4ddd84e42c05bef987472d8b6
Gabriel Moreira <gabriel.figueiredo534@gmail.com>
2024-05-28 13:38:46 -0300

    Primeiro Commit

---
+++ src/main/java/org/rbg/npcpixelmon/NpcPixelmon.java
@@ -0,0 +1,17 @@
+package org.rbg.npcpixelmon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NpcPixelmon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
