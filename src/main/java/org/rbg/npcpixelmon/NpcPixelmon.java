7df53015de7a5c933be78016c350d9d647a79734
Tiago Ribolli <ribollitiago@gmail.com>
2024-05-28 13:49:20 -0300

    add onenable

--- src/main/java/org/rbg/npcpixelmon/NpcPixelmon.java
+++ src/main/java/org/rbg/npcpixelmon/NpcPixelmon.java
@@ -2,16 +2,22 @@ package org.rbg.npcpixelmon;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class NpcPixelmon extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getLogger().info("Plugin CustomNPC ativado!");
+        try {
+            Objects.requireNonNull(getCommand("criarnpc")).setExecutor(new CreateNPCCommand(this));
+        } catch (NullPointerException e) {
+            getLogger().warning("O comando 'criarnpc' não foi encontrado! Verifique se está definido corretamente no plugin.yml.");
+        }
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
