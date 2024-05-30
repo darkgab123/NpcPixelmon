2ae0d4138b734d1d4141c17d870fd912e4ecd3e9
Tiago Ribolli <ribollitiago@gmail.com>
2024-05-30 14:39:59 -0300

    Finalizado deletar npc

--- src/main/java/org/rbg/npcpixelmon/commands/DeleteNPC.java
+++ src/main/java/org/rbg/npcpixelmon/commands/DeleteNPC.java
@@ -1,5 +1,7 @@
 package org.rbg.npcpixelmon.commands;
 
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.npc.NPC;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -14,11 +16,30 @@ public class DeleteNPC implements Listener {
     }
 
     public static boolean deleteNPCCommand(CommandSender sender, Player p, String[] args){
-        String id = args[1];
-        FileConfiguration config = plugin.getConfig();
 
+        String idString = args[1];
+        int id;
+
+        try {
+            id = Integer.parseInt(idString);
+        } catch (NumberFormatException e) {
+            sender.sendMessage("§cID inválido. Deve ser um número.");
+            return false;
+        }
+
+        NPC npc = CitizensAPI.getNPCRegistry().getById(id);
+        if (npc == null) {
+            sender.sendMessage("§cNPC com ID " + id + " não encontrado.");
+            return false;
+        }
+
+        FileConfiguration config = plugin.getConfig();
         config.set("NPCs."  + id, null);
+        npc.despawn();
+        npc.destroy();
+
         sender.sendMessage("§f§eO npc de id: " + id + " §afoi deletado com sucesso.");
+
         return true;
     }
 }
